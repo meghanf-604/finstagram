e46aab987cc238e188418e8287a5cec9f02cfce8
Meghan Fulton <meghanfulton3@gmail.com>
2021-02-09 03:41:46 +0000

    updated forms for login, signup, logout

--- app/actions.rb
+++ app/actions.rb
@@ -1,5 +1,12 @@
+helpers do
+    def current_user
+        User.find_by(id: session[:user_id])
+    end
+end
+
 get '/' do
   @finstagram_posts = FinstagramPost.order(created_at: :desc)
+#   @current_user = User.find_by(id: session[:user_id])
   erb(:index)
 end
 
@@ -17,8 +24,34 @@ post '/signup' do
   @user = User.new({ email: email, avatar_url: avatar_url, username: username, password: password })
 
   if @user.save
-    "User #{username} saved!"
+    redirect to ('/login')
   else
     erb(:signup)
   end
+end
+
+get '/login' do
+    erb(:login)
+end
+
+post '/login' do
+    username = params[:username]
+    password = params[:password]
+
+    user = User.find_by(username: username)
+
+    # if user && user.password == password
+    if user && user.authenticate(password)
+        session[:user_id] = user.id
+        redirect to('/')
+        # "Success! User with id #{session[:user_id]} is logged in!"
+    else
+       @error_message = "Login failed."
+       erb(:login)
+    end
+end 
+
+    get '/logout' do  
+        session[:user_id] = nil
+        redirect to('/')
 end
